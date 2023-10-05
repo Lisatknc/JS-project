7d1804e3c0128e5666ea12e18cb3987574b57847
Lisa Takenouchi <rhrbanb@gmail.com>
2023-10-05 17:15:15 +0900

    "master"

--- 03-reviews/setup/app.js
+++ 03-reviews/setup/app.js
@@ -9,7 +9,7 @@ const reviews = [
   },
   {
     id: 2,
-    name: 'anna johnson',
+    name: 'Sary johnson',
     job: 'web designer',
     img: 'https://www.course-api.com/images/people/person-2.jpeg',
     text: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
@@ -39,46 +39,15 @@ const prevBtn = document.querySelector('.prev-btn');
 const nextBtn = document.querySelector('.next-btn');
 const randomBtn = document.querySelector('.random-btn');
 
-// set starting item
+// set starting item 0index0
 let currentItem = 0;
-
 // load initial item
-window.addEventListener('DOMContentLoaded', function () {
-  const item = reviews[currentItem];
+window.addEventListener('DOMContentLoaded',function(){
+  const item = reviews[currentItem]
+  // reviewsのindex[0]
   img.src = item.img;
+  // htmlのimgタグのsrcに、reviews[0]のimgの中身を入れる
   author.textContent = item.name;
   job.textContent = item.job;
   info.textContent = item.text;
-});
-
-// show person based on item
-function showPerson(person) {
-  const item = reviews[person];
-  img.src = item.img;
-  author.textContent = item.name;
-  job.textContent = item.job;
-  info.textContent = item.text;
-}
-// show next person
-nextBtn.addEventListener('click', function () {
-  currentItem++;
-  if (currentItem > reviews.length - 1) {
-    currentItem = 0;
-  }
-  showPerson(currentItem);
-});
-// show prev person
-prevBtn.addEventListener('click', function () {
-  currentItem--;
-  if (currentItem < 0) {
-    currentItem = reviews.length - 1;
-  }
-  showPerson(currentItem);
-});
-// show random person
-randomBtn.addEventListener('click', function () {
-  console.log('hello');
-
-  currentItem = Math.floor(Math.random() * reviews.length);
-  showPerson(currentItem);
 });
